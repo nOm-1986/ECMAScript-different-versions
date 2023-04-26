eb08c82525c1c09f237e2fcaccb9d1aa1ee3d4e1
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-04-26 17:05:32 -0500

    template literals

---
+++ src/es6/02-strings.js
@@ -0,0 +1,17 @@
+let hello = 'Hello';
+let world = 'World';
+let epicPhrase = hello + ' ' + world;
+console.error(epicPhrase);
+
+/**
+ * Template Literals
+ * Pueden ser Multi-line strings
+ */
+let epicPhrase2 = `${hello} 
+${world}`;
+let multi_line = `Haciendo multi 
+linea utilizando
+template literals`;
+console.log(multi_line);
+
+console.log(epicPhrase2);
