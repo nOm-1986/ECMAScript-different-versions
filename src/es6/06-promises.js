3f6f2d6918ae672aa302bbaff5ff3f280f939988
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-04-27 12:24:00 -0500

    Promises

---
+++ src/es6/06-promises.js
@@ -0,0 +1,19 @@
+/**
+ * Promise
+ * Recordemos el concepto de promesa: algo que va a pasar o pueda que no =D.
+ */
+
+const anotherFunction = () => {
+    //Callback - Funcion que recibe otra funcion y la ejecuta.
+    return new Promise((resolve, reject) => {
+        if(false){
+            resolve('Hey!!, funciona weritooo!!');
+        } else {
+            reject('Whooooops - Lo mando al catch');
+        }
+    });
+}
+
+anotherFunction()
+    .then(response => console.log(response))
+    .catch(err => console.log(err));
