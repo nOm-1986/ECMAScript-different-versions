29e029baedb8458a3756d53385ec2388a62410a4
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-05-10 16:33:29 -0500

    BinInt -> to work with bigest numbers

---
+++ src/es11/01-bigInt.js
@@ -0,0 +1,8 @@
+/**
+ * Cuando trabajamos con números de gran volumen.
+ * 
+ */
+const aBigNumber = 89461989775469879469874n;
+const anotherBigNumber = BigInt(89461989775469879469874);
+console.log(aBigNumber);
+console.log(anotherBigNumber);
