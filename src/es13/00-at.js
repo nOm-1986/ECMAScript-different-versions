953ec5dfea7f0ddc704e6d96888f159934c1ab18
José Fabián Beltrán Meza <joshefbeltran@gmail.com>
2023-05-12 15:23:45 -0500

    At -> To get the last index fromo my array, at[0]

---
+++ src/es13/00-at.js
@@ -0,0 +1,9 @@
+/**
+ * at
+ */
+
+const ar = ["one", "two", "three", "four", 'five', 'six', 'seven', 'eight', 'nine','ten', 'eleven',' twelve'];
+
+//Cómo accedemos al último elemento del array sin saber cuantos elementos tenemos?
+console.log(ar[ar.length - 1]);
+console.log(ar.at(1));
---
+++ src/es13/release.js
@@ -0,0 +1,3 @@
+/**
+ * La siguiente versión de ECMAScript fue publicada en 2022 - ES13.
+*/
